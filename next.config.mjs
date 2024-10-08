75d323e1841815d4aa8221ffe0973d009e6a7288
Alban Caré <alban.care@gmail.com>
2024-10-08 14:22:45 +0000

    Refactor: Update output value in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import createMDX from "@next/mdx";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: process.env.NODE_ENV !== "production" ? "" : "/vrai-architecture",
-  output: process.env.NODE_ENV !== "production" ? undefined : "export",
+  output: "export",
   reactStrictMode: true,
 };
 
